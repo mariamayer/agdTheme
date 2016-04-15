5e6e7d57c8c539b11964f6a31cc6dee512cab52e
Mep <mep@riseup.net>
2016-04-15 19:40:29 -0300

    Pagina recibo de sueldo

--- recibo-sueldo.php
+++ recibo-sueldo.php
@@ -5,7 +5,74 @@ Template name: Recibo de sueldo
 get_header(); ?>
 
 <div class="container body">
+	<div class="col-md-12 bloque">
+		<section class="seccion-area">
+		<span class="subtitulo"> Servicios </span>
+		<h1 class="titulo-pag">RECIBO DE SUELDO</h1>
+		<p class="copete">Los recibos de sueldo de los trabajadores de la Universidad de Buenos Aires deben ser retirados en cada unidad  académica . La AGD sigue reclamando para que, como en otras instituciones del Estado, se pueda acceder vía Internet para facilitar su acceso y el inmediato control del cobro. 
+		El pago vía depósito bancario aumenta las dificultades de los docentes para entender la composición de su salario, por lo tanto alentamos a los docentes a revisar con atención su recibo de sueldo como otro modo de defender su salario. Desarrollamos aquí los elementos mas importantes a tener en cuenta:
+		</p>
 
+		<hr>
+
+		 <img src="http://localhost/sitios/wordpress/agduba/wp-content/uploads/sites/2/2016/04/recibo-sueldo.jpg" alt="Recibo de sueldo"> 
+		<hr>
+		<br>
+		<ul id="toggle-view">
+		<li>
+			<h3>1. Datos personales</h3>
+			<span>+</span>
+			<div class="panel">
+			<p>SECRETARÍA GENERAL : Julio Bulacio 
+				SECRETARÍA GREMIAL: María Julia Loray 
+				SECRETARÍA DE PRENSA: Bernarda Díaz 
+				SECRETARÍA DE HACIENDA Y ADM.: Leonardo Difranza 
+				VOCALÍA TITULAR: María Laura Carboni 
+				VOCALÍA SUPLENTE: Sofía Gastellu </p>
+			</div>
+		</li>
+		<li>
+			<h3>2. Fecha de antiguedad relativa</h3>
+			<span>+</span>
+			<div class="panel">
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim.</p>
+			</div>
+		</li>
+		<li>
+			<h3>3. Cargo</h3>
+			<span>+</span>
+			<div class="panel">
+			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Morbi commodo, ipsum sed pharetra gravida, orci magna rhoncus neque, id pulvinar odio lorem non turpis. Nullam sit amet enim.</p>
+			</div>
+		</li>
+		<li>
+			<h3>4. Haberes / 5.Sueldo bruto</h3>
+			<span>+</span>
+			<div class="panel">
+			<p>SECRETARÍA GENERAL : Julio Bulacio 
+				SECRETARÍA GREMIAL: María Julia Loray 
+				SECRETARÍA DE PRENSA: Bernarda Díaz 
+				SECRETARÍA DE HACIENDA Y ADM.: Leonardo Difranza 
+				VOCALÍA TITULAR: María Laura Carboni 
+				VOCALÍA SUPLENTE: Sofía Gastellu </p>
+			</div>
+		</li>
+		<li>
+			<h3>5. Descuentos / 6.Sueldo neto</h3>
+			<span>+</span>
+			<div class="panel">
+			<p>SECRETARÍA GENERAL : Julio Bulacio 
+				SECRETARÍA GREMIAL: María Julia Loray 
+				SECRETARÍA DE PRENSA: Bernarda Díaz 
+				SECRETARÍA DE HACIENDA Y ADM.: Leonardo Difranza 
+				VOCALÍA TITULAR: María Laura Carboni 
+				VOCALÍA SUPLENTE: Sofía Gastellu </p>
+			</div>
+		</li>
+		</ul>
+		<hr>
+		</section>
+	</div>	
 </div>
 
 <?php get_footer(); ?>
