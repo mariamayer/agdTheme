c147ab903823caceb7d67b7d852b7be2c661a6d7
Mep <mep@riseup.net>
2016-05-27 20:28:24 -0300

    ajuste en contacto

--- contacto.php
+++ contacto.php
@@ -9,12 +9,13 @@ get_header(); ?>
 		<section class="seccion-area">
 		<span class="subtitulo"> AGD UBA </span>
 		<h1 class="titulo-pag">Contacto</h1>
-		<p class="copete">
+		<p class="cuerpo">
 
     Dirección: Chile 2181 CABA <br>
     Tel: 4941 6311/6337<br>
-    Correo electrónico: <a href="mailto:adg@sociales.uba.ar" target="_top">adg@sociales.uba.ar</a> <br>
+    Correo electrónico: <a href="mailto:uba.agd@gmail.com" target="_top">uba.agd@gmail.com</a> <br>
 </p>
+<hr>
 <iframe width="425" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://www.openstreetmap.org/export/embed.html?bbox=-58.401464223861694%2C-34.61981511453243%2C-58.39324593544006%2C-34.61449977335951&amp;layer=mapnik&amp;marker=-34.61715748649454%2C-58.39735507965088" style="border: 1px solid black"></iframe><br/><small><a href="http://www.openstreetmap.org/?mlat=-34.61716&amp;mlon=-58.39736#map=17/-34.61716/-58.39736">Ver mapa más grande</a></small>
 
 		</section>
